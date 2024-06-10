ab5166ecc68e21942d884691ed092a13d6d00729
David Lasry <davidlasry2000@gmail.com>
2024-06-10 14:26:42 +0300

    basic layout

--- src/pages/AlgosPage.jsx
+++ src/pages/AlgosPage.jsx
@@ -1,11 +1,102 @@
-import { Card, Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card, Container } from "react-bootstrap";
+import "./AlgosPage.css";
+import { useNavigate } from "react-router-dom";
 
 const AlgosPage = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+  const navigate = useNavigate();
+
+  const handleCardClick = (cardTitle) => {
+    setSelectedCard(cardTitle);
+  };
+
+  const handleRunClick = () => {
+    console.log(selectedCard);
+    navigate("/output");
+  };
+
   return (
     <Container className="mt-5 pt-5 d-flex flex-column justify-content-center align-items-center">
-      <h1 class="display-6">Please choose a clustering method:</h1>
+      <h1 className="display-6">Please choose a clustering method:</h1>
+      <Container className="d-flex mt-5 justify-content-center gap-4">
+        <Card
+          style={{ width: "18rem" }}
+          onClick={() => handleCardClick("Matisse")}
+          className={selectedCard === "Matisse" ? "selected-card" : ""}
+        >
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Matisse</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card
+          style={{ width: "18rem" }}
+          onClick={() => handleCardClick("Degas")}
+          className={selectedCard === "Degas" ? "selected-card" : ""}
+        >
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Degas</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card
+          style={{ width: "18rem" }}
+          onClick={() => handleCardClick("Click")}
+          className={selectedCard === "Click" ? "selected-card" : ""}
+        >
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Click</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card
+          style={{ width: "18rem" }}
+          onClick={() => handleCardClick("Samba")}
+          className={selectedCard === "Samba" ? "selected-card" : ""}
+        >
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Samba</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Button
+        variant="primary"
+        className="mt-5"
+        size="lg"
+        onClick={handleRunClick}
+      >
+        Run selected clustering method
+      </Button>
       <Container>
-        
+        <Button
+          variant="primary"
+          className="mt-5"
+          size="lg"
+          onClick={() => navigate("/input")}
+        >
+          Back
+        </Button>
       </Container>
     </Container>
   );
--- src/pages/OutputPage.jsx
+++ src/pages/OutputPage.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "./OutputPage.css"
 
 const OutputPage = () => {
   const navigate = useNavigate();
@@ -12,15 +13,19 @@ const OutputPage = () => {
     animate={{ width: "100%" }}
     exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
     >
-      <Container className="mt-5 pt-5 d-flex flex-column justify-content-center align-items-center">
-        This is the Output page
+      <Container className="mt-5 d-flex flex-column justify-content-center align-items-center gap-3">
+        <h2>Data Visualization</h2>
+        <Container className="d-flex align-items-stretch vis_container gap-3">
+          <Container className="border border-2 options_menu">options menu</Container>
+          <Container className="border border-2 visual_results">visual results</Container>
+        </Container>
         <Button
-          onClick={() => navigate("/")}
-          className="mt-5 p-3"
+          onClick={() => navigate("/algocards")}
+          className="mt-5 p-3 align-self-start"
           variant="primary"
           size="lg"
         >
-          Home
+          Back
         </Button>{" "}
       </Container>
     </motion.div>
