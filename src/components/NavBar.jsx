33bb18ee052e318e47ef78b2f16a6d82cc88ceff
David Lasry <davidlasry2000@gmail.com>
2024-06-10 17:59:10 +0300

    building layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <Navbar expand="xl" className="navbar bg-primary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand className='ms-3' href="/">Xpander</Navbar.Brand>
+        <Navbar.Brand className="ms-3" href="/">
+          Xpander
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav >
+          <Nav className="d-flex ms-3 gap-3">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown!!" id="basic-nav-dropdown">
+            <NavDropdown title="Research Groups" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Citations" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -35,11 +48,22 @@ const NavBar = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown title="Repositories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/pages/OutputPage.jsx
+++ src/pages/OutputPage.jsx
@@ -1,23 +1,44 @@
 import { motion } from "framer-motion";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import "./OutputPage.css"
+import Chart from "react-apexcharts";
+import "./OutputPage.css";
 
 const OutputPage = () => {
   const navigate = useNavigate();
 
+  const options = {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
+  };
+
+  const series = [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+  ];
+
   return (
     <motion.div
-    style={{height: "100%"}}
-    initial={{ width: 0 }}
-    animate={{ width: "100%" }}
-    exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+      style={{ height: "100%" }}
+      initial={{ width: 0 }}
+      animate={{ width: "100%" }}
+      exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
     >
       <Container className="mt-5 d-flex flex-column justify-content-center align-items-center gap-3">
         <h2>Data Visualization</h2>
         <Container className="d-flex align-items-stretch vis_container gap-3">
-          <Container className="border border-2 options_menu">options menu</Container>
-          <Container className="border border-2 visual_results">visual results</Container>
+          <Container className="border border-2 options_menu">
+            options menu
+          </Container>
+          <Container className="border border-2 visual_results">
+            <Chart options={options} series={series} type="bar" width="500" />
+          </Container>
         </Container>
         <Button
           onClick={() => navigate("/algocards")}
